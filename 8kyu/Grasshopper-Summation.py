c349988a8eee896407cf7d9dd3f94726028d36cf
lennox909 <lennoxnjenga8093@gmail.com>
2023-08-08 21:32:01 -0500

    added new codewars challenge

---
+++ 8kyu/Grasshopper-Summation.py
@@ -0,0 +1,16 @@
+#Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+#For example (Input -> Output):
+
+#2 -> 3 (1 + 2)
+#8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+
+# My solution 
+
+def summation(num):
+    pass
+    total_sum = 0
+    for i in range(1, num + 1):
+        total_sum += i
+    return total_sum 
+# Code here
